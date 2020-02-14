3f3e26b1316f20976ac177fc7d1fa995e133f43d
Sam Vaughan <sam.vaughan@sydney.edu.au>
2020-02-14 11:49:15 +1100

    Changed plots.py to use object orientated matplotlib interface. Also included three new functions in utils.plots

--- src/koala/utils/plots.py
+++ src/koala/utils/plots.py
@@ -22,20 +22,22 @@ def plot_redshift_peaks(fig_size,
 
     This function plots after the above ^ is performed :)
     """
-    plt.figure(figsize=(fig_size, fig_size / 2.5))
-    plt.plot(wavelength, funcion, "r", lw=1, alpha=0.5)
-    plt.minorticks_on()
-    plt.xlabel("Wavelength [$\AA$]")
-    plt.ylabel("Flux / continuum")
+    fig, ax = plt.subplots(figsize=(fig_size, fig_size / 2.5))
+    ax.plot(wavelength, funcion, "r", lw=1, alpha=0.5)
+    
+    ax.set_xlabel(r"Wavelength [$\AA$]")
+    ax.set_ylabel("Flux / continuum")
 
-    plt.xlim(lmin, lmax)
-    plt.ylim(fmin, fmax)
-    plt.axhline(y=cut, color="k", linestyle=":", alpha=0.5)
+    ax.set_xlim(lmin, lmax)
+    ax.set_ylim(fmin, fmax)
+    ax.axhline(y=cut, color="k", linestyle=":", alpha=0.5)
     for i in range(len(peaks)):
-        plt.axvline(x=peaks[i], color="k", linestyle=":", alpha=0.5)
+        ax.axvline(x=peaks[i], color="k", linestyle=":", alpha=0.5)
         label = peaks_name[i]
-        plt.text(peaks[i], 1.8, label)
+        ax.text(peaks[i], 1.8, label)
+    
     plt.show()
+    return fig
 
 
 def plot_weights_for_getting_smooth_spectrum(wlm,
@@ -52,36 +54,32 @@ def plot_weights_for_getting_smooth_spectrum(wlm,
     Weights for getting smooth spectrum
     """
     fig_size = 12
-    plt.figure(figsize=(fig_size, fig_size / 2.5))
-    plt.plot(wlm, s, alpha=0.5)
-    plt.plot(running_wave, running_step_median, "+", ms=15, mew=3)
-    plt.plot(wlm, fit_median, label="fit median")
-    plt.plot(wlm, fit_median_interpolated, label="fit median_interp")
-    plt.plot(
-        wlm,
-        weight_fit_median * fit_median
-        + (1 - weight_fit_median) * fit_median_interpolated,
-        label="weighted",
-    )
+    fig, ax = plt.subplots(figsize=(fig_size, fig_size / 2.5))
+    ax.plot(wlm, s, alpha=0.5)
+    ax.plot(running_wave, running_step_median, "+", ms=15, mew=3)
+    ax.plot(wlm, fit_median, label="fit median")
+    ax.plot(wlm, fit_median_interpolated, label="fit median_interp")
+    ax.plot(wlm, weight_fit_median * fit_median + (1 - weight_fit_median) * fit_median_interpolated, label="weighted")
+
     extra_display = old_div((np.nanmax(fit_median) - np.nanmin(fit_median)), 10)
-    plt.ylim(
+    ax.set_ylim(
         np.nanmin(fit_median) - extra_display, np.nanmax(fit_median) + extra_display
     )
-    plt.xlim(wlm[0] - 10, wlm[-1] + 10)
-    plt.minorticks_on()
-    plt.legend(frameon=False, loc=1, ncol=1)
+    ax.set_xlim(wlm[0] - 10, wlm[-1] + 10)
+    ax.tick_params(axis='both', which='minor')
+    ax.legend(frameon=False, loc=1, ncol=1)
 
-    plt.axvline(x=wave_min, color="k", linestyle="--")
-    plt.axvline(x=wave_max, color="k", linestyle="--")
+    ax.axvline(x=wave_min, color="k", linestyle="--")
+    ax.axvline(x=wave_max, color="k", linestyle="--")
 
-    plt.xlabel("Wavelength [$\AA$]")
+    ax.set_xlabel(r"Wavelength [$\AA$]")
 
     if exclude_wlm[0][0] != 0:
         for i in range(len(exclude_wlm)):
-            plt.axvspan(exclude_wlm[i][0], exclude_wlm[i][1], color="r", alpha=0.1)
+            ax.axvspan(exclude_wlm[i][0], exclude_wlm[i][1], color="r", alpha=0.1)
 
     plt.show()
-    plt.close()
+    return fig
 
 
 def plot_correction_in_fibre_p_fibre(fig_size,
@@ -94,15 +92,15 @@ def plot_correction_in_fibre_p_fibre(fig_size,
     """
     Plot correction in fibre p_fibre
     """
-    plt.figure(figsize=(fig_size, fig_size / 2.5))
-    plt.plot(
+    fig, ax = plt.subplots(figsize=(fig_size, fig_size / 2.5))
+    ax.plot(
         wlm,
         old_div(espectro_old, espectro_fit_median),
         "r",
         label="Uncorrected",
         alpha=0.5,
     )
-    plt.plot(
+    ax.plot(
         wlm,
         old_div(espectro_new, espectro_fit_median),
         "b",
@@ -110,22 +108,22 @@ def plot_correction_in_fibre_p_fibre(fig_size,
         alpha=0.5,
     )
     const = old_div((np.nanmax(espectro_new) - np.nanmin(espectro_new)), 2)
-    plt.plot(
+    ax.plot(
         wlm,
         old_div((const + espectro_new - espectro_old), espectro_fit_median),
         "k",
         label="Dif + const",
         alpha=0.5,
     )
-    plt.axhline(y=clip_high, color="k", linestyle=":", alpha=0.5)
-    plt.ylabel("Flux / Continuum")
+    ax.axhline(y=clip_high, color="k", linestyle=":", alpha=0.5)
+    ax.set_ylabel("Flux / Continuum")
 
-    plt.xlabel("Wavelength [$\AA$]")
-    plt.title("Checking correction in fibre " + str(fibre_p))
-    plt.legend(frameon=False, loc=1, ncol=4)
-    plt.minorticks_on()
+    ax.set_xlabel(r"Wavelength [$\AA$]")
+    ax.set_title("Checking correction in fibre {}".format(str(fibre_p)))
+    ax.legend(frameon=False, loc=1, ncol=4)
+
     plt.show()
-    plt.close()
+    return fig
 
 
 def plot_suspicious_fibres(suspicious_fibres,
@@ -136,28 +134,25 @@ def plot_suspicious_fibres(suspicious_fibres,
     """
     Plotting suspicious fibres
     """
+    figures = []
     for fibre in suspicious_fibres:
-        plt.figure(figsize=(fig_size, fig_size / 2.5))
-        plt.minorticks_on()
-        plt.xlabel("Wavelength [$\AA$]")
-        plt.ylabel("Relative Flux")
-        plt.xlim(wave_min, wave_max)
+        fig, ax = plt.subplots(figsize=(fig_size, fig_size / 2.5))
+        ax.tick_params(axis='both', which='minor')
+        ax.set_xlabel(r"Wavelength [$\AA$]")
+        ax.set_ylabel("Relative Flux")
+        ax.set_xlim(wave_min, wave_max)
         scale = np.nanmax(intensity_corrected_fiber[fibre]) - np.nanmin(
             intensity_corrected_fiber[fibre]
         )
-        plt.ylim(
+        ax.set_ylim(
             np.nanmin(intensity_corrected_fiber[fibre]) - old_div(scale, 15),
             np.nanmax(intensity_corrected_fiber[fibre]) + old_div(scale, 15),
         )
-        text = (
-            "Checking spectrum of suspicious fibre "
-            + np.str(fibre)
-            + ". Do you see a cosmic?"
-        )
-        plt.title(text)
+        ax.set_title("Checking spectrum of suspicious fibre {}. Do you see a cosmic?".format(np.str(fibre)))
         self.plot_spectrum(fibre)
         plt.show()
-        plt.close()
+        figures.append(fig)
+    return figures
 
 
 def plot_skyline_5578(fig_size,
@@ -166,96 +161,66 @@ def plot_skyline_5578(fig_size,
     """
     Checking throughput correction using skyline 5578
     """
-    plt.figure(figsize=(fig_size, fig_size / 2.5))
-    plt.plot(flux_5578, alpha=0.5)
-    plt.plot(flux_5578_medfilt, alpha=1.0)
-    plt.ylabel("Integrated flux of skyline 5578 $\AA$")
-    plt.xlabel("Fibre")
+    fig, ax = plt.subplots(figsize=(fig_size, fig_size / 2.5))
+    ax.plot(flux_5578, alpha=0.5)
+    ax.plot(flux_5578_medfilt, alpha=1.0)
+    ax.set_ylabel(r"Integrated flux of skyline 5578 $\AA$")
+    ax.set_xlabel("Fibre")
     p01 = np.nanpercentile(flux_5578, 1)
     p99 = np.nanpercentile(flux_5578, 99)
-    plt.ylim(p01, p99)
-    plt.title("Checking throughput correction using skyline 5578 $\AA$")
-    plt.minorticks_on()
+    ax.set_ylim(p01, p99)
+    ax.set_title(r"Checking throughput correction using skyline 5578 $\AA$")
+    ax.tick_params(axis='both', which='minor')
     plt.show()
-    plt.close()
+
+    return fig
 
 
-def plot_spec(w, 
-              f, 
-              size=0):
+def plot_offset_between_cubes(cube, x, y, wl, medfilt_window=151):
+
     """
-    Plot spectrum given wavelength, w, and flux, f.
-    """              
-    if size != 0:
-        plt.figure(figsize=(size, size / 2.5))
-    plt.plot(w, f)
-    return
+    Plot the offset between two cubes
 
-
-def plot_plot(
-    x,
-    y,
-    xmin=0,
-    xmax=0,
-    ymin=0,
-    ymax=0,
-    ptitle="Pretty plot",
-    xlabel="Wavelength [$\AA$]",
-    ylabel="Flux [counts]",
-    fcal=False,
-    save_file="",
-    frameon=False,
-    loc=0,
-    ncol=4,
-    fig_size=0,
-):
+    Args:
+        cube (Cube object): A cube instance
+        x (array): ?
+        y (array): ?
+        wl (array): A wavelenhgth array
+        medfilt_window (int): Window size for median filtering
     """
-    Plot beautiful spectrum 
-    """  
 
-    if fig_size != 0:
-        plt.figure(figsize=(fig_size, fig_size / 2.5))
-        plt.plot(x, y)
+    smooth_x = signal.medfilt(x, medfilt_window)
+    smooth_y = signal.medfilt(y, medfilt_window)
 
-    if xmin == 0:
-        xmin = np.nanmin(x)
-    if xmax == 0:
-        xmax = np.nanmax(x)
-    if ymin == 0:
-        ymin = np.nanmin(y)
-    if ymax == 0:
-        ymax = np.nanmax(y)
+    print(np.nanmean(smooth_x))
+    print(np.nanmean(smooth_y))
 
-    plt.xlim(xmin, xmax)
-    plt.ylim(ymin, ymax)
-    plt.title(ptitle)
-    plt.minorticks_on()
-    if loc != 0:
-        plt.legend(frameon=frameon, loc=loc, ncol=ncol)
-    plt.xlabel(xlabel)
-    if fcal:
-        ylabel = "Flux [ erg cm$^{-2}$ s$^{-1}$ A$^{-1}$]"
-    plt.ylabel(ylabel)
-
-    if save_file == "":
-        plt.show()
-    else:
-        plt.savefig(save_file)
-    plt.close()
-
-
+    fig, ax = plt.subplots(figsize=(10, 5))
+    wl = cube.RSS.wavelength
+    ax.plot(wl, x, "k.", alpha=0.1)
+    ax.plot(wl, y, "r.", alpha=0.1)
+    ax.plot(wl, smooth_x, "k-")
+    ax.plot(wl, smooth_y, "r-")
+    #    plt.plot(wl, x_max-np.nanmedian(x_max), 'g-')
+    #    plt.plot(wl, y_max-np.nanmedian(y_max), 'y-')
+    ax.set_ylim(-1.6, 1.6)
+    plt.show()
+    return fig
 
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
-def plot_response(calibration_star_cubes, 
-                  scale=[1, 1, 1, 1]):
+def plot_response(calibration_star_cubes, scale=[1, 1, 1, 1]):
+
     """
-    Plot response curves for calibration stars.
-    """                   
-            
+    Plot the response of standard stars. TODO rename this function to make it more explicit
+
+    Args:
+        calibration_star_cubes (list): A list of standard star objects
+        scale (list): ?
+    """
+
     print("\n> Plotting response of standard stars...\n")
-
-    plt.figure(figsize=(11, 8))
+    fig, ax = plt.subplots(figsize=(11, 8))
     wavelength = calibration_star_cubes[0].wavelength
     mean_curve = np.zeros_like(wavelength)
     mean_values = []
@@ -266,17 +231,16 @@ def plot_response(calibration_star_cubes,
         R = star.response_curve[good] * scale[i]
         mean_curve += np.interp(wavelength, wl, R)
         star.response_full = np.interp(wavelength, wl, R)
-        plt.plot(
+        ax.plot(
             star.response_wavelength,
             star.response_curve * scale[i],
             label=star.description,
             alpha=0.5,
             linewidth=2,
         )
-        print("  Mean value for ", star.object, " = ", np.nanmean(
-            star.response_curve * scale[i]
-        ), "      scale = ", scale[i])
-        mean_values.append(np.nanmean(star.response_curve) * scale[i])
+        mean_value = star.response_curve * scale[i]
+        print("\tMean value for {} = {}, scale = {}".format(star.object, np.nanmean(mean_value), scale[i]))
+        mean_values.append(mean_value)
         i = i + 1
     mean_curve /= len(calibration_star_cubes)
 
@@ -285,12 +249,12 @@ def plot_response(calibration_star_cubes,
         response_rms += np.abs(star.response_full - mean_curve)
     response_rms /= len(calibration_star_cubes)
     dispersion = old_div(np.nansum(response_rms), np.nansum(mean_curve))
-    print("  Variation in flux calibrations =  {:.2f} %".format(dispersion * 100.0))
+    print("\tVariation in flux calibrations =  {:.2f} %".format(dispersion * 100.0))
 
     # dispersion=np.nanmax(mean_values)-np.nanmin(mean_values)
     # print "  Variation in flux calibrations =  {:.2f} %".format(dispersion/np.nanmedian(mean_values)*100.)
 
-    plt.plot(
+    ax.plot(
         wavelength,
         mean_curve,
         "k",
@@ -298,10 +262,38 @@ def plot_response(calibration_star_cubes,
         alpha=0.2,
         linewidth=10,
     )
-    plt.legend(frameon=False, loc=2)
-    plt.ylabel("FLux")
-    plt.xlabel("Wavelength [$\AA$]")
-    plt.title("Response curve for calibration stars")
+    ax.legend(frameon=False, loc=2)
+    ax.set_ylabel("Flux")
+    ax.set_xlabel(r"Wavelength [$\AA$]")
+    ax.set_title("Response curve for calibration stars")
+    ax.tick_params(axis='both', which='minor')
+    plt.show()
+
+    return fig
+
+
+def plot_telluric_correction(wlm, telluric_correction_list, telluric_correction, figsize=12):
+    """
+    Make a plot of the telluric corretion as well as all of the individual stars which have gone into this telluric correction.
+
+    Args:
+        wlm (array): Wavelength array
+        telluric_correction_list (list): A list of standard star spectra
+        telluric_correction (array): The final telluric correction we'll use
+        figsize (int, default=12): Size of the figure in inches
+
+    Returns:
+        A matplotlib figure object
+    """
+
+    fig, ax = plt.subplots(figsize=(fig_size, fig_size / 2.5))
+    ax.set_title("Telluric correction")
+
+    for i in range(len(telluric_correction_list)):
+        label = r"star {}".format(i + 1)
+        plt.plot(wlm, telluric_correction_list[i], alpha=0.3, label=label)
+
+    ax.plot(wlm, telluric_correction, alpha=0.5, color="k", label="Median")
     plt.minorticks_on()
     plt.show()
 
