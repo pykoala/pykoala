481e9547cfb08e310913dd41765b391eacf9c47b
PabloCorcho <corchocaballeropablo@gmail.com>
2022-02-09 10:50:33 +1100

    Mere changes on path management

--- src/koala/pykoala_processing_example.py
+++ src/koala/pykoala_processing_example.py
@@ -42,10 +42,11 @@ if __name__ == "__main__":
     # # That will be where your data are.
     # # 
     # # Type where your data will be:
-    path = "/DATA/KOALA/Python/GitHub/test_reduce/"
-    #path = os.path.join('..', '..', 'tests', 'reduced_data')
-    #if not os.path.isdir(path):
-    #    os.mkdir('../../tests/reduced_data')
+    #path = "/DATA/KOALA/Python/GitHub/test_reduce/"
+
+    path = os.path.join('..', '..', 'tests', 'reduced_data')
+    if not os.path.isdir(path):
+        os.mkdir('../../tests/reduced_data')
 
     # # If needed, you can copy the example data using this:        
     #os.system("mkdir "+path)
