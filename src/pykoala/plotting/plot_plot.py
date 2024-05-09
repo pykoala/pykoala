69ac164115c158bf80339f117e43996b852f939b
angelrls <Angel.Lopez-Sanchez@mq.edu.au>
2024-05-09 16:45:05 +1000

    Updated plot_plot

--- src/pykoala/plotting/plot_plot.py
+++ src/pykoala/plotting/plot_plot.py
@@ -3,52 +3,22 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
-def full_path(filename,path, verbose=False):
-    """
-    Check if string variable filename has the full path to that file. If it hasn't it adds the path.
-
-    Parameters
-    ----------
-    filename : string
-        Name of a file.
-    path : string
-        Full path to file filename
-    verbose : Boolean 
-
-    Returns
-    -------
-    fullpath : string
-        The file with the full path
-    """   
-    if path != "": 
-        if path[-1] != "/" : path = path+"/" # If path does not end in "/" it is added
-        
-        if len(filename.replace("/","")) == len(filename):
-            if verbose: print("\n> Variable {} does not include the full path {}".format(filename,path))
-            fullpath = path+filename
-        else:
-            if verbose: print("\n> Variable {} includes the full path {}".format(filename,path))
-            fullpath = filename
-    else:
-        fullpath = filename
-        if verbose: print("  The path has not been given, returning {} ...".format(filename))
-    return fullpath
-# -----------------------------------------------------------------------------
-# -----------------------------------------------------------------------------
-# -----------------------------------------------------------------------------
-def plot_plot(x, y,  xmin=None,xmax=None,ymin=None,ymax=None,percentile_min=2, percentile_max=98,
+def plot_plot(x, y,  xmin=None,xmax=None,ymin=None,ymax=None,percentile_min=2, percentile_max=98, extra_y = 0.1,
               ptitle = None, xlabel = None, ylabel = None, label="",
               #ptitle="Pretty plot", xlabel="Wavelength [$\mathrm{\AA}$]", ylabel="", 
               fcal=None, 
-              psym="", color="blue", alpha="", linewidth=1,  linestyle="-", markersize = 10,
-              vlines=[], hlines=[], chlines=[], cvlines=[], axvspan=[[0,0]], caxvspan = [],
+              psym=None, color=None, alpha=None, linewidth=1,  linestyle=None, markersize = 10,
+              vlines=[], hlines=[], chlines=[], cvlines=[], 
+              caxvspan = None, axvspan=None, #[[0,0]], 
               hwidth =1, vwidth =1,
               frameon = False, loc = 0, ncol = 6,   text=[],
               title_fontsize=12, label_axes_fontsize=10, axes_fontsize=10, tick_size=[5,1,2,1], axes_thickness =0,
-              save_file="", path="", fig_size=9, warnings = True, show=True, statistics=""):
+              save_file=None, path=None, fig_size=9, warnings = True, verbose=True,
+              show=True, statistics="", plot=True, **kwargs):
     
    """
    Plot this plot! An easy way of plotting plots in Python.
@@ -63,6 +33,8 @@ def plot_plot(x, y,  xmin=None,xmax=None,ymin=None,ymax=None,percentile_min=2, p
         Lower bound percentile for filtering outliers in data
     percentile_max : integer (default = 98)
         Higher bound percentile for filtering outliers in data
+    extra_y : float between 0 and 1
+        Extra space up and down in y-axis, percentage of the plotting interval (ymax-ymin)
     ptitle : string (default = "Pretty plot")
         Title of the plot
     xlabel : string (default = "Wavelength [$\mathrm{\AA}$]")
@@ -132,6 +104,9 @@ def plot_plot(x, y,  xmin=None,xmax=None,ymin=None,ymax=None,percentile_min=2, p
         Print statistics of the data in the console
        
    """
+   
+   if color is None: color = "blue"
+   if linestyle is None: linestyle ="-"
       
    if fig_size == "big":
        fig_size=20
@@ -153,11 +128,12 @@ def plot_plot(x, y,  xmin=None,xmax=None,ymin=None,ymax=None,percentile_min=2, p
        hwidth =4 
        vwidth =4
 
-   if fig_size != 0 : 
-       if type(fig_size) == list:
-           plt.figure(figsize=(fig_size[0], fig_size[1]))
-       else:     
-           plt.figure(figsize=(fig_size, fig_size/2.5))
+   if fig_size not in ["C","c","continue","Continue"]:
+       if fig_size != 0 : 
+           if type(fig_size) == list:
+               plt.figure(figsize=(fig_size[0], fig_size[1]))
+           else:     
+               plt.figure(figsize=(fig_size, fig_size/2.5))
    
    if np.isscalar(x[0]) :
        xx=[]
@@ -170,7 +146,6 @@ def plot_plot(x, y,  xmin=None,xmax=None,ymin=None,ymax=None,percentile_min=2, p
    if xmax is None : xmax = np.nanmax(xx[0])  
    
    alpha_=alpha
-   psym_=psym
    label_=label
    linewidth_=linewidth
    markersize_=markersize
@@ -179,27 +154,26 @@ def plot_plot(x, y,  xmin=None,xmax=None,ymin=None,ymax=None,percentile_min=2, p
    n_plots=len(y)
        
    if np.isscalar(y[0]) ==  False:
-       if np.isscalar(alpha):        
-           if alpha_ == "":
-               alpha =[0.5]*n_plots
-           else:
-               alpha=[alpha_]*n_plots
-       if np.isscalar(psym): psym=[psym_]*n_plots
+               
+       if alpha_ is None:
+           alpha =[0.5]*n_plots
+       elif np.isscalar(alpha):
+           alpha=[alpha_]*n_plots
+       if psym is None: psym=[None]*n_plots
        if np.isscalar(label): label=[label_]*n_plots
        if np.isscalar(linewidth): linewidth=[linewidth_]*n_plots
        if np.isscalar(markersize):markersize=[markersize_]*n_plots
        if np.isscalar(linestyle): linestyle=[linestyle_]*n_plots
        if color == "blue" : 
-           color_list = ["red","blue","green","k","orange", "purple", "cyan", "lime"]
+           color_list = ["red","blue","green","k","orange", "purple", "cyan", "lime", "plum","teal","lightcoral", "navy"]
            color = []
            j=0
            for i in range(n_plots):
                color.append(color_list[j])
                j = j+1
-               if j == 8 : j = 0
-           
-           #color = [color_list[i] for i in range(n_plots)]
-       if ymax is None: y_max_list = []
+               if j == 12 : j = 0
+       if ymax is None: 
+           y_max_list = []
        if ymin is None: y_min_list = []
               
        if fcal is None:
@@ -207,25 +181,17 @@ def plot_plot(x, y,  xmin=None,xmax=None,ymin=None,ymax=None,percentile_min=2, p
                fcal = True
                if np.nanmedian(y[0]) < 1E-20 and np.var(y[0]) > 0 : fcal = False
        for i in range(len(y)):
-           if psym[i] == "":
-               plt.plot(xx[i],y[i], color=color[i], alpha=alpha[i], label=label[i], linewidth=linewidth[i], linestyle=linestyle[i])
-           else:
-               #print(xx)
-               #print(y)
-               # print(psym)
-               # print(color) 
-               # print(alpha)
-               # print(label)
-               # print(linewidth)
-               # print(markersize)
-               # print(len(xx),len(y), len(psym), len(color), len(alpha), len(label), len(linewidth), len(markersize))
+           #print("color=",color,"\n alpha=", alpha,"\n label=", label, "linewidth=", linewidth, "linestyle=", linestyle)
+           if psym[i] is None:
+             plt.plot(xx[i],y[i], color=color[i], alpha=alpha[i], label=label[i], linewidth=linewidth[i], linestyle=linestyle[i])
+           else: 
                plt.plot(xx[i],y[i], psym[i], color=color[i], alpha=alpha[i], label=label[i], mew=linewidth[i], markersize=markersize[i])
 
-           if ymax == "":
+           if ymax is None:
                     y_max_ = []                
                     y_max_.extend((y[i][j]) for j in range(len(xx[i])) if (xx[i][j] > xmin and xx[i][j] < xmax) )  
                     y_max_list.append(np.nanpercentile(y_max_, percentile_max))
-           if ymin == "":
+           if ymin is None:
                     y_min_ = []                
                     y_min_.extend((y[i][j]) for j in range(len(xx[i])) if (xx[i][j] > xmin and xx[i][j] < xmax) )  
                     y_min_list.append(np.nanpercentile(y_min_, percentile_min))
@@ -234,13 +200,13 @@ def plot_plot(x, y,  xmin=None,xmax=None,ymin=None,ymax=None,percentile_min=2, p
        if ymin is None:
            ymin = np.nanmin(y_min_list)
    else:
-       if alpha == "": alpha=1
-       if statistics =="": statistics = True
+       if alpha is None: alpha=1
+       if statistics == None : statistics = True
        if fcal is None:
            if np.nanmedian(np.abs(y)) < 1E-10 : 
                fcal= True 
                if np.nanmedian(np.abs(y)) < 1E-20 and np.nanvar(np.abs(y)) > 0 : fcal = False
-       if psym == "":
+       if psym is None:
              plt.plot(x,y, color=color, alpha=alpha,linewidth=linewidth,  linestyle=linestyle)
        else:
            plt.plot(x,y, psym, color=color, alpha=alpha, mew=linewidth, markersize=markersize)
@@ -253,10 +219,19 @@ def plot_plot(x, y,  xmin=None,xmax=None,ymin=None,ymax=None,percentile_min=2, p
            y_max_.extend((y[j]) for j in range(len(x)) if (x[j] > xmin and x[j] < xmax) )  
            ymax = np.nanpercentile(y_max_, percentile_max)    
        
-   plt.xlim(xmin,xmax)                    
-   plt.ylim(ymin,ymax)
-   if ptitle is None: ptitle="Pretty plot"
+
+   # Provide extra range up and down
+   if extra_y is not None:
+       interval = (ymax - ymin)
+       ymin = ymin - interval * extra_y
+       ymax = ymax + interval * extra_y
    
+   # Set limits
+   plt.xlim(xmin,xmax)
+   plt.ylim(ymin,ymax)
+   
+   # Set title
+   if ptitle is None: ptitle="Pretty plot"
    try:   
        plt.title(ptitle, fontsize=title_fontsize)
    except Exception:
@@ -312,10 +287,8 @@ def plot_plot(x, y,  xmin=None,xmax=None,ymin=None,ymax=None,percentile_min=2, p
    if label_ != "" : 
        plt.legend(frameon=frameon, loc=loc, ncol=ncol)
        
-   if axvspan[0][0] != 0:
-       if len(axvspan) != len(caxvspan):
-           for i in range(len(axvspan)-len(caxvspan)):
-               caxvspan.append("orange") 
+   if axvspan is not None:
+       if caxvspan is None:  caxvspan = ["orange" for item in axvspan]
        for i in range(len(axvspan)):
            plt.axvspan(axvspan[i][0], axvspan[i][1], facecolor=caxvspan[i], alpha=0.15, zorder=3)   
            
@@ -323,17 +296,17 @@ def plot_plot(x, y,  xmin=None,xmax=None,ymin=None,ymax=None,percentile_min=2, p
         for i in range(len(text)):
             plt.text(text[i][0],text[i][1], text[i][2], size=axes_fontsize)
                     
-   if save_file == "":
+   if save_file == None:
        if show: 
            plt.show()
            plt.close() 
    else:
-       if path != "" : save_file=full_path(save_file,path)
+       if path != None : save_file=os.path.join(path, save_file) 
        plt.savefig(save_file)
        plt.close() 
-       print("  Figure saved in file",save_file)
+       if verbose: print("  Figure saved in file",save_file)
    
-   if statistics == "": statistics=False
+   if statistics == None: statistics=False
    if statistics:
        if np.isscalar(y[0]) : 
            basic_statistics(y, x=x, xmin=xmin, xmax=xmax)
@@ -343,16 +316,16 @@ def plot_plot(x, y,  xmin=None,xmax=None,ymin=None,ymax=None,percentile_min=2, p
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
-def basic_statistics(y, x="", xmin="", xmax="", return_data=False, verbose = True):
+def basic_statistics(y, x=None, xmin=None, xmax=None, return_data=False, verbose = True):
     """
     Provides basic statistics: min, median, max, std, rms, and snr"
     """    
-    if len(x) == 0:
+    if x is None:
         y_ = y
     else:          
         y_ = []      
-        if xmin == "" : xmin = x[0]
-        if xmax == "" : xmax = x[-1]          
+        if xmin is None : xmin = x[0]
+        if xmax is None : xmax = x[-1]          
         y_.extend((y[j]) for j in range(len(x)) if (x[j] > xmin and x[j] < xmax) )  
      
     median_value = np.nanmedian(y_)
@@ -360,9 +333,7 @@ def basic_statistics(y, x="", xmin="", xmax="", return_data=False, verbose = Tru
     max_value = np.nanmax(y_)
     
     n_ = len(y_)
-    #mean_ = np.sum(y_) / n_
     mean_ = np.nanmean(y_)
-    #var_ = np.sum((item - mean_)**2 for item in y_) / (n_ - 1)  
     var_ = np.nanvar(y_)
 
     std = np.sqrt(var_)
