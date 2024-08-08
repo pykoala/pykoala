80ae842f1ad4de45fc7c9d74c37936210d1996b6
pcorchoc <p.corcho.caballero@rug.nl>
2024-08-08 17:21:35 +0200

    refactor default logger

--- src/pykoala/ancillary.py
+++ src/pykoala/ancillary.py
@@ -14,7 +14,7 @@ from matplotlib import pyplot as plt
 from scipy import interpolate
 from scipy import optimize
 import logging
-
+import sys
 # =============================================================================
 # Astropy and associated packages
 # =============================================================================
@@ -25,21 +25,24 @@ from astropy.io import fits
 # PyKOALA modules
 # =============================================================================
 
-logger = logging.getLogger('pykoala.logger')
+# Parent logger
+pykoala_logger = logging.getLogger('pykoala')
 
-if not (logger.hasHandlers()):
-    stdout = logging.StreamHandler()
+if not (pykoala_logger.hasHandlers()):
+    stdout = logging.StreamHandler(sys.stdout)
     fmt = logging.Formatter(
-    "[PyKOALA] %(asctime)s | %(levelname)s > %(message)s")   
+    "[%(name)s] %(asctime)s|%(levelname)s> %(message)s",
+    datefmt="%Y/%m/%d %H:%M")
     stdout.setFormatter(fmt)
-    logger.addHandler(stdout)
-    logger.setLevel(logging.INFO)
+    pykoala_logger.addHandler(stdout)
+    pykoala_logger.setLevel(logging.INFO)
 
-def log_into_file(filename, level='INFO'):
-    logger = logging.getLogger('pykoala.logger')
+def log_into_file(filename, logger_name='pykoala', level='INFO'):
+    logger = logging.getLogger(logger_name)
     hdlr = logging.FileHandler(filename)
     fmt = logging.Formatter(
-    "[PyKOALA] %(asctime)s | %(levelname)s > %(message)s")   
+    "[%(name)s] %(asctime)s|%(levelname)s> %(message)s",
+    datefmt="%Y/%m/%d %H:%M")
     hdlr.setFormatter(fmt)
     logger.addHandler(hdlr) 
     logger.setLevel(level)
