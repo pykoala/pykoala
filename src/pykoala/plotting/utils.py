afcb7960f28033b586717667f752ef85651a7cf7
Pablo Corcho-Caballero <p.corcho.caballero@rug.nl>
2024-08-24 15:00:26 +0200

    add astropy visualization normalization

--- src/pykoala/plotting/utils.py
+++ src/pykoala/plotting/utils.py
@@ -4,9 +4,9 @@ from matplotlib.ticker import AutoMinorLocator
 
 import numpy as np
 
-# Needed?
-# from astropy.visualization import quantity_support
-# quantity_support()
+from astropy.visualization import (MinMaxInterval, PercentileInterval,
+                                   SqrtStretch, PowerStretch,
+                                   ImageNormalize, AsymmetricPercentileInterval)
 
 
 def new_figure(fig_name,
@@ -48,7 +48,7 @@ def new_figure(fig_name,
     fig = plt.figure(fig_name, figsize=figsize)
     axes = fig.subplots(nrows=nrows, ncols=ncols, squeeze=False,
                         sharex=sharex, sharey=sharey,
-                        gridspec_kw=gridspec_kw
+                        gridspec_kw=gridspec_kw,
                         )
     # fig.set_tight_layout(True)
     for ax in axes.flat:
@@ -71,10 +71,14 @@ default_cmap.set_bad('gray')
 
 
 def colour_map(fig, ax, cblabel, data,
-               cmap=default_cmap, norm=None,
+               cmap=default_cmap,
                xlabel=None, x=None,
                ylabel=None, y=None,
-               cbax=None):
+               cbax=None, norm=None,
+               norm_interval=AsymmetricPercentileInterval,
+               interval_args={"lower_percentile": 1.0,
+                              "upper_percentile": 99.0},
+               stretch=PowerStretch, stretch_args={"a": 0.7}):
     """
     Plot a colour map (imshow) with axes and colour scales.
 
@@ -106,16 +110,14 @@ def colour_map(fig, ax, cblabel, data,
     im : mpl.AxesImage
     cb : mpl.Colorbar
     """
-
     if norm is None:
-        percentiles = np.array([1, 16, 50, 84, 99])
-        ticks = np.nanpercentile(data, percentiles)
-        linthresh = np.median(data[data > 0])
-        norm = colors.SymLogNorm(vmin=2*ticks[0]-ticks[1],
-                                 vmax=2*ticks[-1]-ticks[-2],
-                                 linthresh=linthresh)
-    else:
-        ticks = None
+        interval = norm_interval(**interval_args)
+        norm = ImageNormalize(data, interval=interval,
+                              stretch=stretch(**stretch_args),
+                              clip=False)
+    elif isinstance(norm, str):
+        norm = getattr(colors, norm)
+
     if y is None:
         y = np.arange(data.shape[0])
     if x is None:
@@ -135,19 +137,16 @@ def colour_map(fig, ax, cblabel, data,
         ax.set_ylabel(ylabel)
 
     if cbax is None:
-        cb = fig.colorbar(im, ax=ax, orientation='vertical', shrink=.9)
+        cb = fig.colorbar(im, ax=ax, orientation='vertical', shrink=.9,
+                          extend='both')
         cbax = cb.ax
     elif cbax:
-        cb = fig.colorbar(im, cax=cbax, orientation='vertical')
+        cb = fig.colorbar(im, cax=cbax, orientation='vertical', extend='both')
     else:
         cb = None
     if cbax:
         cb.ax.yaxis.set_label_position("left")
         cb.set_label(cblabel)
-        if ticks is not None:
-            cb.ax.set_yticks(ticks=ticks, labels=[f'{value:.3g} ({percent}\\%)'
-                                                  for value, percent in
-                                                  zip(ticks, percentiles)])
         cb.ax.tick_params(labelsize='small')
 
     return im, cb
