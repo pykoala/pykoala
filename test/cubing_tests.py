7d6c6700788322946923bc7e7ef62b81952803e5
P. Corcho-Caballero <corchocaballeropablo@gmail.com>
2025-01-20 09:21:43 +0100

    remove astrometric offsets

--- test/cubing_tests.py
+++ test/cubing_tests.py
@@ -1,11 +1,9 @@
 import unittest
 import os
-import numpy as np
 
 from astropy import units as u
 from astropy.wcs import WCS
 
-from pykoala.data_container import RSS
 from pykoala.instruments.mock import mock_rss
 from pykoala import cubing
 
@@ -22,10 +20,10 @@ class TestCubing(unittest.TestCase):
         print("Setting up RSS objects for testing")
         self.rss_1 = mock_rss(ra_cen=180 << u.deg,
                               dec_cen=45 << u.deg)
-        self.rss_2 = mock_rss(ra_cen=180 << u.deg + 0.3 << u.arcsec,
+        self.rss_2 = mock_rss(ra_cen=180 << u.deg,
                               dec_cen=45 << u.deg)
-        self.rss_3 = mock_rss(ra_cen=180 << u.deg + 0.3 << u.arcsec,
-                              dec_cen=45 << u.deg + -0.3 << u.arcsec)
+        self.rss_3 = mock_rss(ra_cen=180 << u.deg,
+                              dec_cen=45 << u.deg)
         self.rss_list = [self.rss_1, self.rss_2, self.rss_3]
 
     def test_cubing(self):
