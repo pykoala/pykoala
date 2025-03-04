d69611f6ec0bd7d6210fb25f8a737abf1a1363d8
Pablo Corcho-Caballero <p.corcho.caballero@rug.nl>
2025-03-04 07:34:07 +0100

    add stacking draft test

--- test/cubing_tests.py
+++ test/cubing_tests.py
@@ -7,6 +7,9 @@ from astropy.wcs import WCS
 from pykoala.instruments.mock import mock_rss
 from pykoala import cubing
 from pykoala.plotting.utils import qc_cube
+from pykoala.corrections.atmospheric_corrections import get_adr
+
+np.random.seed(50)
 
 class TestKernel(unittest.TestCase):
     """Unit tests for cheking the available interpolation kernels."""
@@ -25,8 +28,23 @@ class TestKernel(unittest.TestCase):
         self.assertTrue(kernel.cmf(1.0) == 1.0)
 
 
+class TestStacking(unittest.TestCase):
+
+    def test_cube_stacking(self):
+        _cube_data = np.random.rand(3, 100, 20, 20)
+        _var = (0.05 * _cube_data)**2
+
+        t_sigma = cubing.CubeStacking.sigma_clipping(
+            cubes = _cube_data, variances = _var)
+        t_mad = cubing.CubeStacking.mad_clipping(
+            cubes = _cube_data, variances = _var)
+
+        #TODO: add asserts
+
+
 class TestInterpolation(unittest.TestCase):
     pass
+
 
 class TestCubing(unittest.TestCase):
 
@@ -48,6 +66,12 @@ class TestCubing(unittest.TestCase):
         self.rss_list = [self.rss_1, self.rss_2, self.rss_3]
 
     def test_cubing(self, save=False):
+        # Estimate the Differential Atmospheric Refraction
+        adr_corr_set = []
+        for rss in self.rss_list:
+            adr_corr_set.append(
+                [np.random.normal(0, 0.3, size=rss.wavelength.size) << u.arcsec,
+                 np.random.normal(0, 0.3, size=rss.wavelength.size) << u.arcsec])
         # Build WCS from the list of RSS
         wcs = cubing.build_wcs_from_rss(self.rss_list,
                                         spatial_pix_size=1.0 << u.arcsec,
@@ -55,30 +79,37 @@ class TestCubing(unittest.TestCase):
         assert wcs.has_celestial, "WCS has not celestial axis"
         assert wcs.has_spectral, "WCS has not spectral axis"
 
-        cube, interm_prod = cubing.build_cube(self.rss_list, wcs=wcs,
-                                              kernel_size_arcsec=1.0,
-                                              qc_plots=True,
-                                              keep_individual_cubes=True)
+        interpolator = cubing.CubeInterpolator(self.rss_list, wcs=wcs,
+                                               kernel_size_arcsec=2.0,
+                                               kernel=cubing.DrizzlingKernel,
+                                               adr_set=adr_corr_set,
+                                               qc_plots=True,
+                                               keep_individual_cubes=True)
+        cube = interpolator.build_cube()
         # Save the cube to a FITS file
         cube.to_fits(filename="test_cube.fits",
                      overwrite=True)
         # Load the cube from the FITS
         cube = cubing.Cube.from_fits("test_cube.fits")
         if save:
-            interm_prod["stack_cube"].savefig(
+            # Save QC plots
+            interpolator.cube_plots["stack_cube"].savefig(
                 f"./cube_qcplot_cubing_test.png",
                 bbox_inches="tight", dpi=200)
-            for name, prod in interm_prod.items():
-                if "rss" in name:
-                    prod.savefig(f"./{name}_cubing_test.png",
+            interpolator.cube_plots["weights"].savefig(
+                f"./weights_cubing_test.png",
+                bbox_inches="tight", dpi=200)
+
+            for rss_n, prod in interpolator.rss_inter_products.items():
+                # Fibre-coverage QC plot
+                prod["qc_fibres_on_fov"].savefig(
+                    f"./{rss_n}_cubing_test.png",
+                    bbox_inches="tight", dpi=200)
+                # RSS individual cube
+                rss_cube, rss_cube_fig = prod["cube"]
+                rss_cube_fig.savefig(f"./single_{rss_n}_cube_qc.png",
                                 bbox_inches="tight", dpi=200)
-                elif "cube_" in name:
-                    print(prod)
-                    single_exp_cube, fig = prod
-                    fig.savefig(f"./single_{name}_cube_qc.png",
-                                bbox_inches="tight", dpi=200)
-                    single_exp_cube.to_fits(filename=f"{name}.fits",
-                     overwrite=True)
+                rss_cube.to_fits(filename=f"{rss_n}.fits", overwrite=True)    
         else:
             os.unlink("test_cube.fits")
 
